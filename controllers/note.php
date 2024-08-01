a8c13118b3f7a740a52b187e26bdd00da2f6987f
Luca I <121231127+LuckyLukic@users.noreply.github.com>
2024-08-01 23:29:13 +0200

    refactor

--- controllers/note.php
+++ controllers/note.php
@@ -6,16 +6,9 @@ $db = new Database($config['database']);
 $heading = 'Note';
 $currentUserId = 1;
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-if (!$note) {
-    abort();
-}
+authorize($note['user_id'] === $currentUserId);
 
-
-
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
 
 require "views/note.view.php";
